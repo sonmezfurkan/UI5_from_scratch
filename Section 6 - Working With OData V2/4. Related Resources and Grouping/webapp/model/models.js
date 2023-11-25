d45156047e8dce807d9534d23146c721f5176c65
Furkan Sönmez <sonmez@Furkans-Mini.fritz.box>
2023-11-25 18:45:45 +0100

    Adds OData V2 chapters

---
+++ Section 6 - Working With OData V2/4. Related Resources and Grouping/webapp/model/models.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/BindingMode"
+], function(JSONModel, BindingMode) {
+  return {
+    createInputModel() {
+      return new JSONModel({
+        Name: "",
+        Category: "",
+        Price: "",
+        ReleaseDate: null,
+        DiscontinuedDate: null,
+      }).setDefaultBindingMode(BindingMode.TwoWay)
+    },
+
+    createValidationModel() {
+      return new JSONModel({
+        Name: true,
+        Category: true,
+        Price: true,
+        ReleaseDate: true,
+        DiscontinuedDate: true,
+      }).setDefaultBindingMode(BindingMode.TwoWay)
+    }
+  }
+})
